e0df234120bdfc97d89da2678c97108b85b4642b
Felipe Trajano <ftrajano@gmail.com>
2025-04-23 00:10:20 -0300

    ajustes para deploy

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,34 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+  
+//   // Configuração para o servidor de desenvolvimento
+//   webpack: (config) => {
+//     // Configuração para resolver problemas com MongoDB no ambiente de desenvolvimento
+//     return {
+//       ...config,
+//       experiments: {
+//         topLevelAwait: true,
+//       },
+//     }
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
   // Configuração para o servidor de desenvolvimento
   webpack: (config) => {
     // Configuração para resolver problemas com MongoDB no ambiente de desenvolvimento
     return {
       ...config,
       experiments: {
+        ...config.experiments,  // Mantém experimentos que já estão configurados pelo Next.js
         topLevelAwait: true,
+        layers: true,  // Adiciona suporte para layers, resolvendo o erro
       },
     }
   },
